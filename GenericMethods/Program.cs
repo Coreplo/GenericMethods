d625f36df0440b74c0f570e35e3e915e5bc632b3
Coreplo <coreplo@gmail.com>
2019-07-01 20:37:55 +0200

    generics

--- GenericMethods/Program.cs
+++ GenericMethods/Program.cs
@@ -6,16 +6,50 @@ using System.Threading.Tasks;
 
 namespace GenericMethods
 {
+    //Generics allow the reuse of code across different types
+
     class Program
     {
+        //this code will work ONLY for int types
+        static void SwapInt (ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        //this code will work with MANY types
+        static void SwapGeneric<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        //T is the name of our generic type
+        // the <> brackets are used to define a generic type
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Generic Methods! yay");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            int a = 4, b = 9;
+            Console.WriteLine("{0} - {1}", a, b);
+            SwapGeneric<int>(ref a, ref b);
+            Console.WriteLine("{0} - {1}", a, b);
+
+            string s1 = "hello", s2 = "world";
+            Console.WriteLine("{0} - {1}", s1, s2);
+            SwapGeneric<string>(ref s1, ref s2);
+            Console.WriteLine("{0} - {1}", s1, s2);
+
+
+            Console.ReadKey();
         }
+        // without generics we would have had to overload the mthod 
+        // for every type we want to use
+        // multiple generic params can be used if needed
+        //eg Func<T,U>
+
     }
 }
